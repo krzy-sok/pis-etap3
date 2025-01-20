6422f96e36e5d7e7eee2a2857d08b0a505470b3c
krzy-sok <01178595@pw.edu.pl>
2025-01-20 12:27:40 +0100

    failing test

--- mysite/tests/test_example.py
+++ mysite/tests/test_example.py
@@ -2,7 +2,7 @@ import unittest
 
 class TestAdd(unittest.TestCase):
     def test_simple_addition(self):
-        self.assertEqual(2+2, 4)
+        self.assertEqual(2+2, 5)
 
 if __name__ == '__main__':
     unittest.main()
